e578f2fb2f03173f3ad59c516af4d18ae2bfefad
Kyurem102 <67395482+Kyurem102@users.noreply.github.com>
2024-08-05 01:12:21 -0500

    Update 6872265039.lua

--- CustomModules/6872265039.lua
+++ CustomModules/6872265039.lua
@@ -40,7 +40,7 @@ for _, v in pairs(_G.gc) do
 end
 
 function requireV2(obj)
-    if typeof(obj) == "Instance" and obj:IsA("ModuleScript") then
+    if typeof(obj) == "Instance" and obj:IsA("ModuleScript") or not obj then
         return setmetatable({}, {
             __index = function(obj, key)
                 if _G.gc then
