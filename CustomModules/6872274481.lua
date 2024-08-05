e82cc3e97467a9185cf9262d4113a6350a93e844
Kyurem102 <67395482+Kyurem102@users.noreply.github.com>
2024-08-05 03:55:46 -0500

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -1195,12 +1195,12 @@ run(function()
 	end
 
 	local function dumpRemote(tab)
-		for i,v in pairs(tab) do
-			if v == "Client" then
-				return tab[i + 1]
-			end
-		end
-		return ""
+	    for i,v in pairs(tab) do
+	        if v == "Client" then
+	            return tab[i - 1]
+	        end
+	    end
+	    return ""
 	end
 
 	local KnitGotten, KnitClient
