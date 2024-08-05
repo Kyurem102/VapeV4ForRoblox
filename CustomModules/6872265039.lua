32a1ef279107750343eb1f315cb9f649dd23216a
Kyurem102 <67395482+Kyurem102@users.noreply.github.com>
2024-08-05 03:31:31 -0500

    Update 6872265039.lua

--- CustomModules/6872265039.lua
+++ CustomModules/6872265039.lua
@@ -40,22 +40,28 @@ for _, v in pairs(_G.gc) do
     end
 end
 
-local MTTable = setmetatable({}, {
-    __index = function(obj, key)
-	if _G.gc then
-	    for _, v in pairs(_G.gc) do
-		if type(v) == "table" and rawget(v, key) ~= nil and (type(rawget(v, key)) == "table" or type(rawget(v, key)) == "function") then
-		    return rawget(v, key)
-		end
-	    end
-	end
-	return
-    end
-})
-
-function requireV2(obj)
+function requireV2(obj, targetKey)
     if typeof(obj) == "Instance" and obj:IsA("ModuleScript") or not obj then
-        return MTTable
+        return setmetatable({}, {
+            __index = function(obj, key)
+                if _G.gc then
+                    if targetKey then
+                        for _, v in pairs(_G.gc) do
+                            if type(v) == "table" and rawget(v, targetKey) ~= nil and (type(rawget(v, targetKey)) == "table" or type(rawget(v, targetKey)) == "function") then
+                                return v
+                            end
+                        end
+                    else
+                        for _, v in pairs(_G.gc) do
+                            if type(v) == "table" and rawget(v, key) ~= nil and (type(rawget(v, key)) == "table" or type(rawget(v, key)) == "function") then
+                                return rawget(v, key)
+                            end
+                        end
+                    end
+                end
+                return
+            end
+        })
     end
 
     return require(obj)
